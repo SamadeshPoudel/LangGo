8f989f18ba2891b6b2b7097168926f5d07f85856
Samadesh Poudel <poudelsamadesh@gmail.com>
2025-06-05 16:06:42 +0530

    Completed signup route

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,5 +1,53 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
 export async function signup(req, res){
-    res.send("welcome to signup")
+    const {email, password, fullName} = req.body;
+
+    try {
+        if(!email || !password || !fullName){
+           return res.status(400).json({msg:"All fields are required"}); 
+        }
+        if(password.length < 6){
+            return res.status(400).json({msg:"Password must be at least 6 characters"})
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email)) {
+        return res.status(400).json({ message: "Invalid email format" });
+        }
+
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({msg:"Email already exists, please use a different one"})
+        }
+
+        const index = Math.floor(Math.random()*100)+1;
+        const randomAvatar = `https://avatar.iran.liara.run/public/${index}.png`
+
+        const newUser = await User.create({
+            email,
+            fullName,
+            password,
+            profilePic: randomAvatar,
+        })
+
+        const token = jwt.sign({userId:newUser._id},process.env.JWT_SECRET_KEY, {
+            expiresIn: "7d"
+        })
+        res.cookie("jwt", token,{
+            maxAge:7*24*60*60*1000,
+            httpOnly: true, //prevents xss attacks
+            sameSite: "strict", //prevent csrf attacks
+            secure: process.env.NODE_ENV === "production"
+        })
+
+        res.status(201).json({success:true, user:newUser})
+
+    } catch (error) {
+        console.log("Error in signup controller", error)
+        res.status(500).json({msg:"Internal server error"})
+    }
 }
 
 export async function login(req, res){

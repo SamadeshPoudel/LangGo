ea858e41520ef1de9a7f6337e9a462caf2842c60
Samadesh Poudel <poudelsamadesh@gmail.com>
2025-06-05 14:38:40 +0530

    Added routes and db models

---
+++ backend/src/controller/auth.controller.js
@@ -0,0 +1,11 @@
+export async function signup(req, res){
+    res.send("welcome to signup")
+}
+
+export async function login(req, res){
+    res.send("welcome to login")
+}
+
+export async function logout(req, res){
+    res.send("welcome to logout")
+}
